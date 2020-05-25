846b74e3685c6318967039c21544f4d4d800a3cc
Vincent Ahn <jmanoba@hotmail.com>
2020-05-25 15:05:42 -0400

    Add Overlay Content Add name and description content in overlay with appropriate CSS.

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,21 @@
 function openOverlay(){
+  document.querySelector(".overlay").style.transitionDelay = "0";
   document.querySelector(".overlay").style.width = "100%";
+
+  console.log(document.querySelector(".overlay-content").style.transitionDelay);
+
+  document.querySelector(".overlay-content").style.transitionDelay = "0.75s";
+  document.querySelector(".overlay-content").style.opacity = "1.0";
 }
 
 function closeOverlay(){
+  document.querySelector(".overlay-content").style.transitionDelay = "0";
+  document.querySelector(".overlay").style.transitionDelay = "0.75s";
   document.querySelector(".overlay").style.width = 0;
+  document.querySelector(".overlay-content").style.transitionDelay = "0";
+
+  console.log(document.querySelector(".overlay-content").style.transitionDelay);
+
+  document.querySelector(".overlay-content").style.transitionDelay = "0";
+  document.querySelector(".overlay-content").style.opacity = "0";
 }

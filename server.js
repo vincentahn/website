12c3420ea620371bbe8faa2d3ee0b14a8d73645e
Vincent Ahn <jmanoba@hotmail.com>
2020-07-13 19:50:57 -0400

    Changes to Deploy to Heroku Initialize npm to generate package.json. Edit port to read from environment so it can be deployed onto heroku.

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ app.get("/", function(req, res){
   res.render("home");
 });
 
-app.listen(3000, function(){
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+}
+
+app.listen(port, function(){
   console.log("Server started on port 3000");
 });
